d761131d00151e9726a7ce28f1b66757d1184927
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-03-11 19:45:46 +0100

    trigger

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -2,16 +2,19 @@
 set -euo pipefail
 
 echo -e "[debug] Setting Envs"
-CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+
+# CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 KUBEVAL_VERSION="0.16.1"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 echo -e "[debug] Installing Kubeval ${KUBEVAL_VERSION}"
+
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
 
 echo -e "[debug] Running helm kubevalidation"
+
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
   echo -e "[debug] Validating ${CHART_DIR}"
