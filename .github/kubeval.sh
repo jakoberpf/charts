32624ae6b4e02d58822e6c3609621478376977cb
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-03-11 19:44:14 +0100

    update kubeval script

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
+echo -e "[debug] Setting Envs"
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 KUBEVAL_VERSION="0.16.1"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
+echo -e "[debug] Installing Kubeval ${KUBEVAL_VERSION}"
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
 
+echo -e "[debug] Running helm kubevalidation"
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
+  echo -e "[debug] Validating ${CHART_DIR}"
   helm template "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
 done
