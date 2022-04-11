f7435207e921621a24b2d2a88ebb6473fb8f3b0c
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-04-11 11:52:47 +0200

    add echos

--- .github/verify-istio.sh
+++ .github/verify-istio.sh
@@ -21,6 +21,11 @@ for CHART_DIR in ${CHART_DIRS}; do
 
   echo "Running IstioGateway (HTTP) Test"
   curl https://google.com
+
+  echo ${INGRESS_DNS}
+  echo ${INGRESS_HOST}
+  echo ${INGRESS_PORT}
+  
   curl -s -I -HHost:${INGRESS_DNS} "http://${INGRESS_HOST}:${INGRESS_PORT}"
   CODE=$(curl --write-out %{http_code} --output /dev/null -s -I -HHost:${INGRESS_DNS} "http://${INGRESS_HOST}:${INGRESS_PORT}")
 
