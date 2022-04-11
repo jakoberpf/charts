ca0719702afc74e3077dec05bb8e53abc1bd2439
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-04-11 16:23:49 +0200

    generate certs insde charts dirs

--- .github/tls.sh
+++ .github/tls.sh
@@ -10,6 +10,9 @@ openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/O=example In
 for CHART_DIR in ${CHART_DIRS}; do
   CHART_NAME="$(yq '.name' ${CHART_DIR}/Chart.yaml)"
   
+  mkdir ${CHART_DIR}/.tls
+  cd ${CHART_DIR} .tls
+
   openssl req -out "${CHART_NAME}.csr" -newkey rsa:2048 -nodes -keyout "${CHART_NAME}.key" -subj "/CN=${CHART_NAME}.example.com/O=${CHART_NAME} organization"
   openssl x509 -req -sha256 -days 365 -CA example.com.crt -CAkey example.com.key -set_serial 0 -in "${CHART_NAME}.csr" -out "${CHART_NAME}.crt"
   kubectl create -n istio-system secret tls "${CHART_NAME}-credentials" --key="${CHART_NAME}.key" --cert="${CHART_NAME}.crt"
