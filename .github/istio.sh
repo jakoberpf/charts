6f8888f58ff5abdbfe6534d5fb3ee78162972a49
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-04-11 21:15:33 +0200

    update ssh commands

--- .github/istio.sh
+++ .github/istio.sh
@@ -61,5 +61,5 @@ while [ "$(kubectl get pods -l=app='istio-ingressgateway' -n istio-system -o jso
    echo "Waiting for Istio-IngressGateway to be ready."
 done
 
-ssh -i ~/.minikube/machines/minikube/id_rsa docker@$(minikube ip) -L \*:30080:0.0.0.0:30080
-ssh -i ~/.minikube/machines/minikube/id_rsa docker@$(minikube ip) -L \*:30443:0.0.0.0:30443
+ssh -i ~/.minikube/machines/minikube/id_rsa docker@$(minikube ip) -NL \*:30080:0.0.0.0:30080
+ssh -i ~/.minikube/machines/minikube/id_rsa docker@$(minikube ip) -NL \*:30443:0.0.0.0:30443
