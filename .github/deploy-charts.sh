394d0ef19846cc6bab9a63b8332beba1e0c52bed
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2022-04-28 19:34:33 +0200

    cleanup scripts

--- .github/deploy-charts.sh
+++ .github/deploy-charts.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+GIT_ROOT=$(git rev-parse --show-toplevel)
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 
 # deploy charts
 for CHART_DIR in ${CHART_DIRS}; do
   CHART_NAME="$(yq '.name' ${CHART_DIR}/Chart.yaml)"
-
+  cd ${CHART_DIR}
   if [ ! "$(kubectl get namespaces -o json | jq -r ".items[].metadata.name | select (. == \"${CHART_NAME}\")")" == "${CHART_NAME}" ]; then
     kubectl create namespace "${CHART_NAME}"
   fi
@@ -14,9 +15,8 @@ for CHART_DIR in ${CHART_DIRS}; do
   if [ ! "$(kubectl get namespaces -o json | jq -r ".items[].metadata | select (.name == \"${CHART_NAME}\") | .labels.\"istio-injection\"")" == "enabled" ]; then
     kubectl label namespace "${CHART_NAME}" istio-injection=enabled
   fi
-
   helm dependency build "${CHART_DIR}"
   helm upgrade "${CHART_NAME}" "${CHART_DIR}" --namespace "${CHART_NAME}" --install
-
   .github/wait-until-pods-ready.sh 30 1
+  cd ${GIT_ROOT}
 done
